a482d64e3b8a11313542a06fe438eba2dbd9b48f
Reni Lopez Orias <reni.lopez.orias@est.una.ac.cr>
2020-08-22 13:33:00 -0600

    no message

--- js/Index.js
+++ js/Index.js
@@ -1,3 +1,8 @@
+var sv = $(document).scrollTop();
+console.log();
+
+
+
 
 /*Event's carousel js*/
 new Glider(document.querySelector('.glider'), {
@@ -6,37 +11,34 @@ new Glider(document.querySelector('.glider'), {
     slidesToScroll: 1,
     dots: '#dots',
     arrows: {
-      prev: '.glider-prev',
-      next: '.glider-next'
+        prev: '.glider-prev',
+        next: '.glider-next'
     },
-    responsive: [
-      {
+    responsive: [{
         // screens greater than >= 775px
         breakpoint: 775,
         settings: {
-          // Set to `auto` and provide item width to adjust to viewport
-          slidesToShow: '1.5',
-          slidesToScroll: '1',
-          itemWidth: 150,
-          duration: 0.25
+            // Set to `auto` and provide item width to adjust to viewport
+            slidesToShow: '1.5',
+            slidesToScroll: '1',
+            itemWidth: 150,
+            duration: 0.25
         }
-      },{
+    }, {
         // screens greater than >= 1024px
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2.2,
-          draggable: true,
-          slidesToScroll: 1,
-          itemWidth: 150,
-          duration: 0.25
+            slidesToShow: 2.2,
+            draggable: true,
+            slidesToScroll: 1,
+            itemWidth: 150,
+            duration: 0.25
         }
-      }
-    ]
-  });
+    }]
+});
 
-  /*Event's carousel js*/
-
+/*Event's carousel js*/
 
 
-  /* productos y servicios */
- 
+
+/* productos y servicios */
