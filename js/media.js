1b39a1008dade522cdcac1d82a7d454a11f6aa1e
Luis Enrique Mora <luisenriquemora71829310112@protonmail.com>
2020-08-27 13:33:11 -0600

    haciendo responsive galeria de imagenes en media

--- js/media.js
+++ js/media.js
@@ -1,9 +1,34 @@
 var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    slidesPerColumn: 2,
-    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
     },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1.1,
+        slidesPerColumn: 2,
+        spaceBetween: 10,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1.5,
+        slidesPerColumn: 2,
+        spaceBetween: 10,
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 2.5,
+        slidesPerColumn: 2,
+        spaceBetween: 20,
+      }
+    }
+     
   });
